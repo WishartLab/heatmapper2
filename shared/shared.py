15e63aa4a80e419303af616dffff6ad6003d0faf
Kyle Kernick <kkernick@ualberta.ca>
2024-04-01 18:47:57 -0600

    Make sure Server and Port is defined in WASM

--- shared/shared.py
+++ shared/shared.py
@@ -18,6 +18,7 @@ from enum import Enum
 URL = "https://wishartlab.github.io/heatmapper2"
 Raw = "https://raw.githubusercontent.com/WishartLab/heatmapper2/main"
 
+
 # Detect the running environment
 if "pyodide" in modules:
 	from pyodide.http import pyfetch;
@@ -26,10 +27,11 @@ else:
 	from os.path import exists;
 	Pyodide = False
 
-	# Define the Server and Port of the Shiny instances (Port is incremented)
-	# Change these if Heatmapper is running on a server.
-	Server = "http://127.0.0.1"
-	Port = 8000
+
+# Define the Server and Port of the Shiny instances (Port is incremented)
+# Change these if Heatmapper is running on a server.
+Server = "http://127.0.0.1"
+Port = 8000
 
 
 class ColumnType(Enum): Time = 0; Name = 1; Value = 2; Longitude = 3; Latitude = 4; X = 5; Y = 6; Z = 7; Cluster = 8; Free = 9; Spatial = 10;
