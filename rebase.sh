84fe9fdcb0d52bd74b89f7c83f0c1fe4ad8153ea
Kyle Kernick <kkernick@ualberta.ca>
2024-05-17 10:06:25 -0600

    Shared: Add loading screen text
    It's a little buggy right now, but it only really breaks down at the end, and improvements will be made.

--- rebase.sh
+++ rebase.sh
@@ -7,12 +7,14 @@ done
 
 # Define the list of subfolder names
 subfolders=("expression" "geocoordinate" "geomap" "image" "pairwise")
+stylesheet='    <link rel="stylesheet" href="./../shinylive/heatmapper.css" />'
 
 # Iterate through each subfolder
 for folder in "${subfolders[@]}"; do
     # Check if the subfolder exists
     if [ -d "$folder" ]; then
         shinylive export --subdir $folder $folder/src site
+        sed -i "/<link rel=\"stylesheet\" href=\"\.\/\.\.\/shinylive\/shinylive.css\" \/>/a $stylesheet" "site/$folder/index.html"
     else
         # Print a message if the subfolder doesn't exist
         echo "Subfolder '$folder' not found."
